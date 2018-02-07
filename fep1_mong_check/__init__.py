f88f63cb35db41f3a91df7a21cca2f2d019ee0a7
John ZuHone <jzuhone@gmail.com>
2018-02-07 15:03:29 -0500

    Update signature

--- fep1_mong_check/__init__.py
+++ fep1_mong_check/__init__.py
@@ -1,1 +1,4 @@
 __version__ = "0.1.0"
+
+from .fep1_mong_check import \
+    calc_model
--- fep1_mong_check/fep1_mong_check.py
+++ fep1_mong_check/fep1_mong_check.py
@@ -23,25 +23,19 @@ import sys
 from acis_thermal_check import \
     ACISThermalCheck, \
     calc_off_nom_rolls, \
-    get_options, \
-    make_state_builder, \
-    get_acis_limits
+    get_options
 import os
 
 model_path = os.path.abspath(os.path.dirname(__file__))
 
-yellow_hi, red_hi = get_acis_limits("tmp_fep1_mong")
-
-MSID = {"fep1_mong":'TMP_FEP1_MONG'}
-YELLOW = {"fep1_mong": yellow_hi}
-MARGIN = {"fep1_mong": 2.0}
 VALIDATION_LIMITS = {'TMP_FEP1_MONG': [(1, 2.0), (50, 1.0), (99, 2.0)],
                      'PITCH': [(1, 3.0), (99, 3.0)],
                      'TSCPOS': [(1, 2.5), (99, 2.5)]
                      }
 HIST_LIMIT = [20.]
 
-def calc_model(model_spec, states, start, stop, T_fep=None, T_fep_times=None):
+def calc_model(model_spec, states, start, stop, T_fep=None, T_fep_times=None,
+               dh_heater=None, dh_heater_times=None):
     model = xija.ThermalModel('fep1_mong', start=start, stop=stop,
                               model_spec=model_spec)
     times = np.array([states['tstart'], states['tstop']])
@@ -58,12 +52,11 @@ def calc_model(model_spec, states, start, stop, T_fep=None, T_fep_times=None):
 
 def main():
     args = get_options("fep1_mong", model_path)
-    state_builder = make_state_builder(args.state_builder, args)
-    fep1_mong_check = ACISThermalCheck("tmp_fep1_mong", "fep1_mong", MSID,
-                                       YELLOW, MARGIN, VALIDATION_LIMITS,
-                                       HIST_LIMIT, calc_model)
+    fep1_mong_check = ACISThermalCheck("tmp_fep1_mong", "fep1_mong",
+                                       VALIDATION_LIMITS, HIST_LIMIT,
+                                       calc_model)
     try:
-        fep1_mong_check.driver(args, state_builder)
+        fep1_mong_check.run()
     except Exception as msg:
         if args.traceback:
             raise
