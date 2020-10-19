f6a085e6336e5d7765e52e316cacdd9282ca4557
John ZuHone <jzuhone@gmail.com>
2020-10-19 14:39:25 -0400

    Refactor tests to match new signature

--- fep1_mong_check/tests/test_fep1mong_sql.py
+++ fep1_mong_check/tests/test_fep1mong_sql.py
@@ -3,28 +3,30 @@ from acis_thermal_check.regression_testing import \
     RegressionTester, all_loads
 import pytest
 
-fep1mong_rt = RegressionTester(FEP1MongCheck, model_path, "fep1_mong_test_spec.json")
 
-# SQL state builder tests
-
-fep1mong_rt.run_models(state_builder='sql')
+@pytest.fixture(autouse=True, scope='module')
+def fm_rt(test_root):
+    # SQL state builder tests
+    rt = RegressionTester(FEP1MongCheck, model_path, 
+                          "fep1_mong_test_spec.json",
+                          test_root=test_root, sub_dir='sql')
+    rt.run_models(state_builder='sql')
+    return rt
 
 # Prediction tests
 
-
 @pytest.mark.parametrize('load', all_loads)
-def test_prediction(answer_store, load):
+def test_prediction(fm_rt, answer_store, load):
     if not answer_store:
-        fep1mong_rt.run_test("prediction", load)
+        fm_rt.run_test("prediction", load)
     else:
         pass
 
 # Validation tests
 
-
 @pytest.mark.parametrize('load', all_loads)
-def test_validation(answer_store, load):
+def test_validation(fm_rt, answer_store, load):
     if not answer_store:
-        fep1mong_rt.run_test("validation", load)
+        fm_rt.run_test("validation", load)
     else:
         pass
